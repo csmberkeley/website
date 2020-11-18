7632ba94d548bf5f9511f06f13ade6a3c382b366
noloerino <jhshi07@gmail.com>
2020-11-17 19:16:15 -0800

    Add structure jpg

--- src/pages/Mentors.tsx
+++ src/pages/Mentors.tsx
@@ -31,6 +31,9 @@ export default class Mentors extends React.Component {
                             {Labels.SECTIONS.RESPONSIBILITIES.BODY_JSX}
                         </div>
                     </div>
+                    <img src="/img/CSMStructurePeople.jpg" style={{
+                        width: "60em", marginLeft: "auto", marginRight: "auto", display: "block"
+                    }}/>
                     <div className="divider"></div>
                     <div className="divider"></div>
                     <div className="section">
